2baf415f5eb14387e2f62b0616602cd93e60c32a
Adriaan Mulder <adriaan.mulder@gmail.com>
2018-06-03 00:56:56 -0700

    Only allow admin to update dynamic content.

--- apps/content/views.py
+++ apps/content/views.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .serializers import ContentSerializer
 from .models import Content
+from libs.permissions import IsAdminUserOrReadOnly
 
 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (IsAdminUserOrReadOnly,)
