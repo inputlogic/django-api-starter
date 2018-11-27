ef2315cb73ad3d4e1601009e7ac1ef9c88c2da2a
Gavin Vickery <gavin@geekforbrains.com>
2018-11-27 15:36:09 -0800

    More work on tasks.

--- apps/file/tasks.py
+++ apps/file/tasks.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import requests
 from workers import task
 
 from .models import File
@@ -5,6 +7,10 @@ from .models import File
 
 @task()
 def resize_images():
+    """
+    Get all images to be resized and push into their own tasks
+
+    """
     images = File.images_to_resize()
     for f in images:
         resize_image(f.id)
@@ -12,4 +18,13 @@ def resize_images():
 
 @task()
 def resize_image(file_id):
-    pass
+    """
+    Resize individual image
+
+    """
+    # Open image from url in stream
+    # Resize image in memory (bytes)
+    # Push resized image (bytes) to s3
+    file = File.objects.get(pk=file_id)
+    res = requests.get(file.link, stream=True)
+    im = Image.open(res.raw)
