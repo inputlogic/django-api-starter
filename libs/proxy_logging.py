70c1aebcf5bf4df1b8208bada3019a517b437759
Erik Weisner <eweisner@milly-server.telus>
2019-03-29 16:38:34 -0700

    comment

--- libs/proxy_logging.py
+++ libs/proxy_logging.py
@@ -7,7 +7,7 @@ class ProxyLoggingMixin(LoggingMixin):
     def __get_server_ip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
-            # doesn't even have to be reachable
+            # doesn't have to be reachable
             s.connect(('10.255.255.255', 1))
             ip = s.getsockname()[0]
         except Exception:
