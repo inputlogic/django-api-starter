84e4cc01e0eab75bc53d93870c48d9c10e0178c4
Wes Lord <wes@inputlogic.ca>
2019-04-01 15:11:37 -0700

    Activate browsable REST API

--- project/settings.py
+++ project/settings.py
@@ -202,12 +202,14 @@ WORKERS_PURGE = 1000
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
