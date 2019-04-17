0b3c02a73d3ba66082c7659cec52ab275afb4175
eweisner1973 <erik@inputlogic.ca>
2019-04-17 14:15:07 -0700

    updates (#26)

--- apps/socialmedia/views.py
+++ apps/socialmedia/views.py
@@ -196,7 +196,7 @@ def _googleFlow():
             'client_id': settings.GOOGLE_CLIENT_ID,
             'project_id': settings.GOOGLE_PROJECT_ID,
             'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-            'token_uri': 'https://www.googleapis.com/oauth2/v3/token',
+            'token_uri': 'https://oauth2.googleapis.com/token',
             'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
             'client_secret': settings.GOOGLE_CLIENT_SECRET,
             'redirect_uris': [settings.GOOGLE_REDIRECT_URI]
