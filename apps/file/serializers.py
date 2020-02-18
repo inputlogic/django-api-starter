a1674ef92d4c1474ca468e4b14de7e16e0b6d108
Adrian Unger <dev@staydecent.ca>
2020-02-18 12:36:48 -0800

    Return file id when creating a new file.
    Closes #29

--- apps/file/serializers.py
+++ apps/file/serializers.py
@@ -13,15 +13,17 @@ class FileSerializer(serializers.ModelSerializer):
 class CreateSignedFileSerializer(serializers.Serializer):
     file_name = serializers.CharField(write_only=True)
     acl = serializers.CharField(write_only=True, required=False)
+    file_id = serializers.PrimaryKeyRelatedField(read_only=True)
     url = serializers.URLField(read_only=True)
     content_type = serializers.CharField(read_only=True)
 
     def create(self, validated_data):
         data = get_upload_url(**validated_data)
-        File.objects.create(
+        newfile = File.objects.create(
             user=self.context['request'].user,
             link=data['url'].split('?')[0],  # Only insert actual path, not the signature params
             is_private=validated_data.get('acl') == 'private',
             mime_type=data['content_type']
         )
+        data['file_id'] = newfile.id
         return data
