0457bd65dec37e837fb1725903790ab8cf997af7
Adrian Unger <dev@staydecent.ca>
2020-02-18 12:21:27 -0800

    Remove broken import from Firebase.

--- apps/firebase/models.py
+++ apps/firebase/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from .tasks import update_salesforce_device_id
-
 
 class Firebase(models.Model):
     user = models.ForeignKey(
