01d2682b4804abc6fdbd172e1ca0d51ee1b76782
weslord <wes@inputlogic.ca>
2020-04-16 11:34:37 -0700

    Add test cases for case insenstive email

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -27,6 +27,23 @@ class UserTests(APITestCase):
         self.assertEqual(get_user_model().objects.count(), 1)
         self.assertEqual(get_user_model().objects.get().email, 'test@example.org')
 
+    def test_register_user_icase(self):
+        """
+        User emails should be stored in lowercase
+        """
+        url = reverse('signup')
+        email = 'uppercaseLOWERCASE@lowercaseUPPERCASE.ca'
+        data = {'email': email, 'password': 'yayhooray'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('token', response.data)
+        self.assertIn('userId', response.data)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(
+            get_user_model().objects.get().email,
+            email.lower()
+        )
+
     def test_login_user(self):
         user = get_user_model().objects.create_user(
             email='user@example.com',
@@ -38,13 +55,17 @@ class UserTests(APITestCase):
         self.assertIn('userId', response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
-    def test_get_profile(self):
+    def test_login_user_icase(self):
+        create_email = 'USER@example.com'
+        login_email = 'user@EXAMPLE.com'
         user = get_user_model().objects.create_user(
-            email='user@example.com',
+            email=create_email,
             password='secret',)
-        self.client.force_authenticate(user=user)
-        response = self.client.get(reverse('me'), format='json')
+        url = reverse('login')
+        data = {'email': login_email, 'password': 'secret'}
+        response = self.client.post(url, data, format='json')
+        self.assertIn('token', response.data)
+        self.assertIn('userId', response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_user_can_reset_password(self):
@@ -67,3 +88,25 @@ class UserTests(APITestCase):
         user.refresh_from_db()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(user.check_password(new_password))
+
+    def test_user_can_request_password_icase(self):
+        create_email = 'USER@example.com'
+        reset_email = 'user@EXAMPLE.com'
+        new_password = 'whatever'
+        user = get_user_model().objects.create_user(
+            email=create_email,
+            password='secret',)
+        email_values = []
+        mail.MailResetPassword.send = stub_forgot_password_email(email_values)
+        response = self.client.post(
+            reverse('forgot-password'), data={'email': reset_email}, format='json')
+        self.assertEqual(len(email_values), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_profile(self):
+        user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='secret',)
+        self.client.force_authenticate(user=user)
+        response = self.client.get(reverse('me'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
