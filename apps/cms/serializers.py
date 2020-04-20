8cbb4bc8137e89e984d2b0f0a6eebcb2a481801f
Adrian Unger <dev@staydecent.ca>
2020-04-20 12:13:51 -0700

    Serialize metadata.

--- apps/cms/serializers.py
+++ apps/cms/serializers.py
@@ -3,6 +3,28 @@ from rest_framework import serializers
 from .models import Work, Slide, Page, Section
 
 
+meta_fields = ('meta_title', 'meta_description', 'og_title', 'og_type', 'og_description')
+
+
+class MetadataBaseSerializer(serializers.ModelSerializer):
+    meta_title = serializers.SerializerMethodField()
+    og_title = serializers.SerializerMethodField()
+    meta_description = serializers.SerializerMethodField()
+    og_description = serializers.SerializerMethodField()
+
+    def get_meta_title(self, obj):
+        return obj.meta_title or obj.og_title or obj.title
+
+    def get_og_title(self, obj):
+        return obj.og_title or obj.meta_title or obj.title
+
+    def get_meta_description(self, obj):
+        return obj.meta_description or obj.og_description or ''
+    
+    def get_og_description(self, obj):
+        return obj.og_description or obj.meta_description or ''
+
+
 # Work Posts
 
 
@@ -64,7 +86,7 @@ class SectionSerializer(serializers.ModelSerializer):
         )
 
 
-class PageSerializer(serializers.ModelSerializer):
+class PageSerializer(MetadataBaseSerializer):
     sections = serializers.SerializerMethodField()
 
     class Meta:
@@ -82,7 +104,7 @@ class PageSerializer(serializers.ModelSerializer):
             'parent',
             'created_at',
             'updated_at',
-        )
+        ) + meta_fields
 
     def get_sections(self, page):
         sections = Section.objects.filter(page=page)
@@ -96,4 +118,10 @@ class PageSerializer(serializers.ModelSerializer):
             read_only=True,
         )
         return serializer.data
+
+    def get_meta_description(self, obj):
+        return obj.meta_description or obj.og_description or obj.sub_title or ''
+    
+    def get_og_description(self, obj):
+        return obj.og_description or obj.meta_description or obj.sub_title or ''
 
