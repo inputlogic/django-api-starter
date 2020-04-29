98e86feb875dbfa15097893eccf8b209dcab1e37
Adrian Unger <dev@staydecent.ca>
2020-04-29 15:14:54 -0700

    Add tagged filter.

--- apps/cms/filters.py
+++ apps/cms/filters.py
@@ -3,17 +3,16 @@ from django_filters import rest_framework as filters
 from .models.post import Post
 
 
+class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
+    pass
+
+
 class PostFilters(filters.FilterSet):
-    published_after = filters.IsoDateTimeFilter(
-        field_name="published_on", lookup_expr="gte")
-    published_before = filters.IsoDateTimeFilter(
-        field_name="published_on", lookup_expr="lte")
+    published_after = filters.IsoDateTimeFilter(field_name="published_on", lookup_expr="gte")
+    published_before = filters.IsoDateTimeFilter(field_name="published_on", lookup_expr="lte")
+    tagged = NumberInFilter(field_name='tags__id', lookup_expr='in')
 
     class Meta:
         model = Post
-        fields = {
-            'published_after': [],
-            'published_before': [],
-            'tags__id': ['in'],
-        }
+        fields = ('published_after', 'published_before', 'tagged')
 
