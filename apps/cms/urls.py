a48b11e554246ab9b39fb61b694ffc6f300857a6
Adrian Unger <dev@staydecent.ca>
2020-05-26 16:49:48 -0700

    Add "site-wide" search.
    Endpoint to combine SearchVector's on multiple CMS model types.

--- apps/cms/urls.py
+++ apps/cms/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('posts/<int:pk>', views.PostDetail.as_view(), name='post'),
     path('pages', views.PageList.as_view(), name='page-list'),
     path('pages/<int:pk>', views.PageDetail.as_view(), name='page'),
+    path('search', views.Search.as_view(), name='search'),
 ]
--- apps/cms/views.py
+++ apps/cms/views.py
@@ -1,8 +1,12 @@
+import operator
+import urllib
+
 from datetime import datetime
 
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 
 from .models.post import Post
 from .models.page import Page
@@ -43,3 +47,63 @@ class PageList(generics.ListAPIView):
     serializer_class = PageSerializer
     permission_classes = (permissions.AllowAny,)
     search_fields = ['title', 'sub_title', 'body']
+
+
+class Search(generics.GenericAPIView):
+    """
+    A 'site-wide' search.
+
+    Adapt the `get` method to include more model types.
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request, *args, **kwargs):
+        keywords = request.query_params.get('keywords', '')
+        keywords = urllib.parse.unquote(keywords)
+        ctx = {'request': request}
+        query = SearchQuery(keywords)
+
+        # Posts
+
+        post_vector = (
+            SearchVector('title', weight='A') +
+            SearchVector('body', weight='C')
+        )
+
+        posts = (
+            Post
+            .objects
+            .annotate(rank=SearchRank(post_vector, query))
+            .filter(rank__gte=0.1)
+            .distinct('id')
+        )
+
+        posts = sorted(posts, key=operator.attrgetter('rank'), reverse=True)
+        posts_serializer = PostSerializer(posts, context=ctx, many=True)
+
+        # Pages
+
+        page_vector = (
+            SearchVector('title', weight='A') +
+            SearchVector('sub_title', weight='B') +
+            SearchVector('body', weight='C')
+        )
+
+        pages = (
+            Page
+            .objects
+            .annotate(rank=SearchRank(page_vector, query))
+            .filter(rank__gte=0.1)
+        )
+
+        pages = sorted(pages, key=operator.attrgetter('rank'), reverse=True)
+        pages_serializer = PageSerializer(pages, context=ctx, many=True)
+
+        # Combine into Response
+
+        results = {
+            'posts': posts_serializer.data,
+            'pages': pages_serializer.data,
+        }
+
+        return Response(results, status.HTTP_200_OK)
