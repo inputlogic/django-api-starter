6c70432c142cc3547931cf26ba6ddf1b6d6a7eb9
Gavin Vickery <gavin@geekforbrains.com>
2021-01-28 11:29:03 -0800

    Updating worker pool to handle task status outside of task process.

--- apps/workerexample/tasks.py
+++ apps/workerexample/tasks.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from apps.workers import task
 
 
-#  @task()
+@task()
 def say_hello(name):
     """
     Example of a 'one-off' task that can be called anywhere in you code by importing it.
@@ -21,7 +21,7 @@ def failing_task():
     raise Exception('task failed :(')
 
 
-#  @task(schedule=10)
+@task(schedule=10)
 def say_hello_every_10_seconds():
     """
     Example repeating task that will run every 10 seconds based on the `schedule` param.
@@ -32,7 +32,7 @@ def say_hello_every_10_seconds():
     print('Howdy, its', datetime.utcnow())
 
 
-#  @task(schedule=30)
+@task(schedule=30)
 def say_hello_every_30_seconds():
     """
     Example repeating task that will run every 30 seconds based on the `schedule` param.
@@ -48,4 +48,11 @@ def long_running_task():
     print('start of long running task')
     import time
     time.sleep(30)
+    raise Exception('test fail inside the task')
     print('end of long running task')
+
+
+@task(schedule=30)
+def spawn_hellos_every_10_seconds():
+    for i in range(10):
+        say_hello(f'Task {i}')
--- apps/workers/consumer.py
+++ apps/workers/consumer.py
@@ -1,3 +1,4 @@
+import os
 import json
 import logging
 import signal
@@ -22,31 +23,28 @@ def init_pool():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 
-def process_task(task_id):
-    task = Task.objects.get(id=task_id)
-    module_path, function_name = task.handler.rsplit('.', 1)
-    module = importlib.import_module(module_path)
-    fn = getattr(module, function_name)
-    args = json.loads(task.args)
-    kwargs = json.loads(task.kwargs)
-
-    log.debug(f'running task({task.id}): {task.handler}')
+def process_task(task_id, handler, args, kwargs):
+    log.debug(f'[{os.getpid()}] started task {task_id}')
+    success = False
+    error = 'Task failed without error.'
 
     try:
+        module_path, function_name = handler.rsplit('.', 1)
+        module = importlib.import_module(module_path)
+        fn = getattr(module, function_name)
+        args = json.loads(args)
+        kwargs = json.loads(kwargs)
+
         fn.__wrapped__(*args, **kwargs)
-        task.status = Task.COMPLETED
+        success = True
+        error = ''
     except Exception as e:
-        task.status = Task.FAILED
-        task.error = str(e)
         log.exception(e)
+        success = False
+        error = str(e)
 
-    task.completed_at = timezone.now()
-    task.save()
-
-    if task.schedule:
-        Task.create_scheduled_task(task.handler, task.schedule)
-
-    return task_id
+    log.debug(f'[{os.getpid()}] finished task {task_id} ({success}, {error})')
+    return task_id, success, error
 
 
 def purge_tasks():
@@ -58,10 +56,25 @@ def purge_tasks():
     Task.objects.filter(id__in=tasks).exclude(id__in=tasks[:KEEP]).delete()
 
 
+def handle_callback(result):
+    """
+    Triggered by `pool.apply_async` callback, given result status (str) of the task.
+
+    """
+    (task_id, success, error) = result
+    task = Task.objects.get(pk=task_id)
+    task.status = Task.COMPLETED if success else Task.FAILED
+    task.error = error
+    task.completed_at = timezone.now()
+    task.save()
+
+    if task.schedule:
+        Task.create_scheduled_task(task.handler, task.schedule)
+
+
 def run_forever():
     try:
         with get_context("spawn").Pool(initializer=init_pool, maxtasksperchild=1) as pool:
-            running = []
             while True:
                 with transaction.atomic():
                     task = (
@@ -75,20 +88,21 @@ def run_forever():
                     if task:
                         task.status = Task.RUNNING
                         task.save()
-                        running.append(task.id)
                         pool.apply_async(
                             process_task,
-                            args=(task.id,),
-                            callback=lambda task_id: running.remove(task_id)
+                            args=(task.id, task.handler, task.args, task.kwargs),
+                            callback=handle_callback
                         )
                     else:
                         purge_tasks()  # If we have a sec, cleanup old tasks
                         time.sleep(SLEEP)
-                    log.debug(f'running tasks: {running}')
+                    log.debug('waiting for tasks...')
+
     except (KeyboardInterrupt, SystemExit):
         log.debug('workers stopped, waiting for lingering tasks...')
         pool.close()
         pool.join()
     finally:
-        res = Task.objects.filter(id__in=running).update(status=Task.INCOMPLETE, error='Worker shutdown prematurely.')
-        log.debug(f'incomplete tasks: {res}')
+        (Task.objects
+            .filter(status=Task.RUNNING)
+            .update(status=Task.INCOMPLETE, error='Worker shutdown prematurely.'))
