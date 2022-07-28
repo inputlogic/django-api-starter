b38f16fed26789c3ca2cf6af58a1149a22aa9ded
mjreffel <mjreffel@gmail.com>
2022-07-28 14:07:43 -0700

    Made it so that a task can be called directly and executed immediately by passing in the kwarg now=True

--- apps/workers/broker.py
+++ apps/workers/broker.py
@@ -1,9 +1,26 @@
+import importlib
+import inspect
 import json
+import logging
 from functools import wraps
 
 from django.utils import timezone
 
 from . import registry
+from inspect import getcallargs
+
+log = logging.getLogger(__name__)
+
+
+def run_now(handler, *args, **kwargs):
+    try:
+        module_path, function_name = handler.rsplit('.', 1)
+        module = importlib.import_module(module_path)
+        fn = getattr(module, function_name)
+        results = fn.__wrapped__(*args, **kwargs)
+        return results
+    except Exception as e:
+        log.exception(e)
 
 
 def task(schedule=None, run_at=timezone.now()):
@@ -20,13 +37,18 @@ def task(schedule=None, run_at=timezone.now()):
                 run_at_time = kwargs_run_at
             else:
                 run_at_time = run_at
-            from .models import Task
-            Task.objects.get_or_create(
-                handler=path,
-                args=json.dumps(args),
-                kwargs=json.dumps(kwargs),
-                run_at=run_at_time,
-                status=Task.WAITING
-            )
+
+            if kwargs.pop('now', False):
+                call_args = inspect.getcallargs(f, *args, **kwargs)
+                return run_now(path, *args, **kwargs)
+            else:
+                from .models import Task
+                Task.objects.get_or_create(
+                    handler=path,
+                    args=json.dumps(args),
+                    kwargs=json.dumps(kwargs),
+                    run_at=run_at_time,
+                    status=Task.WAITING
+                )
         return wrapper
     return handler
