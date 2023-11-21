4f59346c99971c55a91cb5f446cffecf2ed68416
Adriaan Mulder <adriaan.mulder@gmail.com>
2023-11-21 10:04:10 -0800

    Update create-signed-file endpoint to return presigned url

--- apps/file/urls.py
+++ apps/file/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('create-signed-file', views.CreateSignedFile.as_view(), name='create-signed-file'),
+    path('user/files/create-signed-file', views.CreateSignedFile.as_view(), name='create-signed-file'),
     path('files', views.UploadFile.as_view(), name='upload-file'),
     path('files/<int:pk>', views.FileDetail.as_view(), name='file-detail'),
 ]
